2a777dc5190dc63bea3f4ce509ba2fb7c12ed9be
jihongkim <jihongeek@kakao.com>
2020-03-13 19:01:39 +0900

    making html with css

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask,render_template,request,send_file
 import datetime
 import re
 import io
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,14 +19,21 @@ def maketicket():
     video_title = request.form['video-title']
     platform = request.form['platform']
     people = request.form['people']
+    place = request.form['place']
+    cssfile = open("static/styles/ticket.css","r")
+    cssstr = "<style>\n" + cssfile.read() + "\n</style>\n"
     html_output = render_template(
             'ticket.html',
             Thistime=Thistime,
             video_title=video_title,
             platform=platform,
-            people=people
+            people=people,
+            place=place
             ) 
-    with open("%s.html"%str(Thistime)[:11], "w") as f:
-        f.write(html_output)
-    return html_output
+    with open("imgmaking/htmlforimg/%s_%d.html"
+    %(str(Thistime)[:11],
+    len(os.listdir("imgmaking/htmlforimg"))+1),"w",
+    encoding="UTF-8") as f:
+        f.write(cssstr + html_output)
+    return cssstr + html_output
 app.run(debug=True)
