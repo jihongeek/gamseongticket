8b1d672ddf36352ffd6eda5ebd62205a9a3fa16f
jihongkim <jihongeek@kakao.com>
2020-03-13 18:21:02 +0900

    make html but witout css

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 #-*- coding: utf-8 -*-
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,send_file
 import datetime
 import re
+import io
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,10 +18,14 @@ def maketicket():
     video_title = request.form['video-title']
     platform = request.form['platform']
     people = request.form['people']
-
-    return render_template('ticket.html',
-        Thistime=Thistime,
-        video_title=video_title,
-        platform=platform,
-        people=people
-        )
+    html_output = render_template(
+            'ticket.html',
+            Thistime=Thistime,
+            video_title=video_title,
+            platform=platform,
+            people=people
+            ) 
+    with open("%s.html"%str(Thistime)[:11], "w") as f:
+        f.write(html_output)
+    return html_output
+app.run(debug=True)
