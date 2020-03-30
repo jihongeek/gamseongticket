ce9f8be296abfe5580c1e68465ce1271c8c080bf
root <root@ip-172-31-34-130.ap-northeast-2.compute.internal>
2020-03-30 14:44:10 +0000

    changed app.py for linux system

---
+++ run.py
@@ -0,0 +1,54 @@
+#-*- coding: utf-8 -*-
+from flask import Flask,render_template,request,send_file
+import datetime
+import re
+import os
+import imgkit
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('template.html')
+
+@app.route('/maketicket',methods={'POST'})
+def maketicket():
+    p = re.compile("[.].+")
+    dt= datetime.datetime.now()
+    dt= p.sub("",str(dt))
+    Thistime = datetime.datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
+    video_title = request.form['video-title']
+    platform = request.form['platform']
+    people = request.form['people']
+    place = request.form['place']
+    theme = request.form['theme']
+    cssfile = open("static/styles/%s.css"%theme,"r")
+    cssstr = "<style>\n" + cssfile.read() + "\n</style>\n"
+    
+    html_output = render_template(
+            'ticket.html',
+            Thistime=Thistime,
+            video_title=video_title,
+            platform=platform,
+            people=people,
+            place=place
+            ) 
+
+    filename = "/%s_%d"%(str(Thistime)[:11],
+    len(os.listdir("imgmaking/htmlforimg"))+1)
+    with open("imgmaking/htmlforimg/"+filename+".html","w",encoding="UTF-8") as f:
+        f.write(cssstr + html_output)
+    options = {
+        'format': 'png',
+        'quality':'100',
+        'encoding':"UTF-8",
+        'crop-h':'500',
+        'crop-w':'500',
+
+        }
+    imgkit.from_file("/var/www/gamseongticket/imgmaking/htmlforimg"+filename+".html",
+    "/var/www/gamseongticket/imgmaking/imgs"+filename+".png",config=config,options=options,)
+    return  send_file("imgmaking/imgs"+filename+".png",
+                     mimetype='image/png',
+                     attachment_filename=filename[1:]+".png",# 다운받아지는 파일 이름. 
+                     as_attachment=True)
+app.run()
